f19dbc96a0110076a89a6290149edf752dd6ea99
pcarofernandez <pcarofernandez@gmail.com>
2022-09-24 18:56:16 +0200

    * Configuration for accessing to Discovery server added in inventory service, order service and product service

--- microservices-new/inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
+++ microservices-new/inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.programmingtechie.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import com.programmingtechie.inventoryservice.model.Inventory;
 import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- microservices-new/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ microservices-new/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
 
 	private final OrderRepository orderRepository;
 
-	private final WebClient webClient;
+	private final WebClient.Builder webClientBuilder;
 
 	public void placeOrder(OrderRequest orderRequest) {
 		Order order = new Order();
@@ -37,8 +37,8 @@ public class OrderService {
 		List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
 		// Call Inventory Service and place order if product is in stock
-		InventoryResponse[] inventoryResponseArray = webClient.get()
-				.uri("http://localhost:8082/api/inventory",
+		InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+				.uri("http://inventory-service/api/inventory",
 						uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
 				.retrieve().bodyToMono(InventoryResponse[].class).block();
 
